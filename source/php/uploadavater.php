1a63bd760c1bee018cd0ce5a1fce5b8003104aa7
lizhe <lizhe@MacBook-Air.local>
2014-03-31 09:00:13 +0800

    v0.2
    v0.2

--- source/php/uploadavater.php
+++ source/php/uploadavater.php
@@ -14,9 +14,9 @@ switch(addcslashes($_GET['action'])){
 		//data[0] 为缩放后的图片 多个大小的会取最大的
 		@file_put_contents('./avatar_small.jpg', $data[0]);
 		//data[1] 为上传的原图
-		@file_put_contents('./avatar.jpg', $data[0]);
+		@file_put_contents('./avatar.jpg', $data[1]);
 		//通过对缩放后的图片按需求在缩放 即可实现按比例缩放并保持剪切等操作
-        $core->cutphoto($big_file, $small_file, 50, 50);
+        //$core->cutphoto($big_file, $small_file, 50, 50);
         
 		$rs['status'] = 1;
 	break;
